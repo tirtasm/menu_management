4252669e845f76a622a9599e557100dab19531ea
tirtasm <tirtasamudraa330@gmail.com>
2024-07-13 16:49:54 +0700

    fix(password): change password done

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -52,12 +52,46 @@
         public function changepassword() {
             $data['user'] = $this->db->get_where('user',
             ['email' => $this->session->userdata['email']])->row_array();
+
+            $this->form_validation->set_rules('currentpassword', 'Current Password', 'required|trim|is_unique[user.password]', [
+                'required' => 'Current Password field is required!',
+                'is_unique' => 'Current Password field does not match!'
+            ]);
+            $this->form_validation->set_rules('newpass', 'New Password', 'required|trim|min_length[4]', [
+                'required' => 'Password field is required!',
+                'min_length' => 'Password field must be at least 4 characters in length.'
+            ]);
+            $this->form_validation->set_rules('repass', 'Repeat Password', 'trim|matches[newpass]', [
+                'required' => 'Repeat Password field is required!',
+                'matches' => 'Repeat Password field does not match!'
+            ]);
             
-            $data['judul'] =  'Change Password';
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('user/index', $data);
-            $this->load->view('templates/footer');
+            if($this->form_validation->run() == false){
+                $data['judul'] =  'Change Password';
+                $this->load->view('templates/header', $data);
+                $this->load->view('templates/sidebar', $data);
+                $this->load->view('templates/topbar', $data);
+                $this->load->view('user/changepassword', $data);
+                $this->load->view('templates/footer');
+            }else{
+                $currentPassword = $this->input->post('currentpassword');
+                $newPassword = $this->input->post('newpass');
+                if(!password_verify($currentPassword, $data['user']['password'])){
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong current password!</div>');
+                    redirect('user/changepassword');
+                }else{
+                    if($currentPassword == $newPassword){
+                        $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">New password cannot be the same as the current password!</div>');
+                        redirect('user/changepassword');
+                    }else{
+                        $passwordHash = password_hash($newPassword, PASSWORD_DEFAULT);
+                        $this->db->where('email', $this->session->userdata('email'));
+                        $this->db->update('user', ['password' => $passwordHash]);
+                        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password changed!</div>');
+                        redirect('user/changepassword');
+                    }
+                }
+            }
+
         }
     }
